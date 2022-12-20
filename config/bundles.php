ee4a9ae76a75814bbf4b0fa31335a42dbee5112e
Artem Rodygin <artem.rodygin@gmail.com>
2022-12-20 22:30:00 +1300

    Web Profiler

--- config/bundles.php
+++ config/bundles.php
@@ -10,5 +10,6 @@ return [
     Symfony\Bundle\MonologBundle\MonologBundle::class                => ['all' => true],
     Symfony\Bundle\SecurityBundle\SecurityBundle::class              => ['all' => true],
     Symfony\Bundle\TwigBundle\TwigBundle::class                      => ['all' => true],
+    Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class        => ['dev' => true, 'test' => true],
     Twig\Extra\TwigExtraBundle\TwigExtraBundle::class                => ['all' => true],
 ];

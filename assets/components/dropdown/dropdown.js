b250d2cfd87007f8ac0bcd669eabb84850583644
Artem Rodygin <artem.rodygin@gmail.com>
2023-01-06 17:42:57 +1300

    [skip ci] Dropdown button

---
+++ assets/components/dropdown/dropdown.js
@@ -0,0 +1,88 @@
+//----------------------------------------------------------------------
+//
+//  Copyright (C) 2017-2023 Artem Rodygin
+//
+//  This file is part of eTraxis.
+//
+//  You should have received a copy of the GNU General Public License
+//  along with eTraxis. If not, see <https://www.gnu.org/licenses/>.
+//
+//----------------------------------------------------------------------
+
+import generateUid from "@utilities/uid";
+
+/**
+ * Dropdown button.
+ */
+export default {
+    props: {
+        /**
+         * @property {string} caption Button text
+         */
+        caption: {
+            type: String,
+            required: true
+        },
+
+        /**
+         * @property {boolean} disabled Whether the dropdown is disabled
+         */
+        disabled: {
+            type: Boolean,
+            default: false
+        }
+    },
+
+    beforeDestroy() {
+        document.removeEventListener("click", this.eventListener);
+    },
+
+    data: () => ({
+        /**
+         * @property {string} uid Unique dropdown ID
+         */
+        uid: generateUid(),
+
+        /**
+         * @property {boolean} isActive Whether the dropdown is active or hidden
+         */
+        isActive: false
+    }),
+
+    methods: {
+        /**
+         * Toggles the dropdown list state.
+         */
+        toggle() {
+            if (!this.disabled) {
+                this.isActive = !this.isActive;
+            }
+        },
+
+        /**
+         * Hides the dropdown menu when user clicks anywhere.
+         *
+         * @param {MouseEvent} event
+         */
+        eventListener(event) {
+            if (!this.$el.querySelector(".dropdown-trigger").contains(event.target)) {
+                this.isActive = false;
+            }
+        }
+    },
+
+    watch: {
+        /**
+         * State of the dropdown menu is changed.
+         *
+         * @param {boolean} value New state
+         */
+        isActive(value) {
+            if (value) {
+                document.addEventListener("click", this.eventListener);
+            } else {
+                document.removeEventListener("click", this.eventListener);
+            }
+        }
+    }
+};
---
+++ assets/components/dropdown/dropdown.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="dropdown" :class="{ 'is-active': isActive }">
+        <div class="dropdown-trigger">
+            <button class="button" :disabled="disabled" :aria-haspopup="!disabled" :aria-controls="`${uid}-dropdown-menu`" @click="toggle">
+                <span>{{ caption }}</span>
+                <span class="icon">
+                    <i class="fa fa-angle-down" aria-hidden="true"></i>
+                </span>
+            </button>
+        </div>
+        <div v-if="!disabled" class="dropdown-menu" :id="`${uid}-dropdown-menu`" role="menu">
+            <div class="dropdown-content">
+                <slot></slot>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script src="./dropdown.js"></script>
